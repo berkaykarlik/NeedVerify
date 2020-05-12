db4d72e179d7e2759cba5872b728214495a9bd67
alptekin <alptekin_ay@hotmail.com>
2020-05-12 21:44:02 +0300

    add route not working version

--- need-verify/index.js
+++ need-verify/index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const logger = require('./util/logger');
@@ -7,6 +8,8 @@ MongoDb.init(process.env.MONGO_URL);
 
 const app = express();
 app.set("port", 3000);
+app.use(bodyParser.urlencoded({ extended : true, limit : "50mb" })); // Use the body-parser package in our application
+app.use(bodyParser.json({ limit : "50mb" }));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api', require('./routes/api'));
