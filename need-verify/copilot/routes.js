34b9f6e6d1b625e9d47a85244046309c6e0d0615
alptekin <alptekin_ay@hotmail.com>
2020-05-16 18:16:38 +0300

    [NV-19] add vue routes

---
+++ need-verify/copilot/routes.js
@@ -0,0 +1,12 @@
+import RegisterView from './src/components/Register.vue'
+
+// Routes
+const routes = [
+  {
+    path: '/register',
+    name: 'Register New Account',
+    component: RegisterView
+  },
+]
+
+export default routes
--- need-verify/copilot/src/main.js
+++ need-verify/copilot/src/main.js
@@ -6,28 +6,31 @@ import 'element-ui/lib/theme-chalk/display.css'
 import locale from 'element-ui/lib/locale/lang/en'
 import ElementUI from 'element-ui'
 import NeedVerifyVueExtensions from './util/NeedVerifyVueExtensions'
-import Register from './components/Register'
+import RegisterView from './components/Register.vue'
+import routes from '../routes'
 import Vue from 'vue'
 import VueResource from "vue-resource"
+import VueRouter from 'vue-router'
 
 Vue.use(ElementUI, { locale })
 Vue.use(VueResource);
+Vue.use(VueRouter);
 Vue.use(NeedVerifyVueExtensions) // Global mixin to extend Vue instance
 Vue.config.productionTip = false
 
-const routes = {
-  '/': Register
-}
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history',
+  scrollBehavior: function (to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 }
+  }
+})
 
 new Vue({
   el: '#app',
   data: {
     currentRoute: window.location.pathname
   },
-  computed: {
-    ViewComponent () {
-      return routes[this.currentRoute]
-    }
-  },
-  render (h) { return h(this.ViewComponent) }
+  router,
+  render: h => h(RegisterView)
 })
