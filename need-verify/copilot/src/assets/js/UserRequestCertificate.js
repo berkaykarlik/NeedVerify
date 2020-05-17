e793b6c11d63836b347b2382630dfd0385ec7014
Burak Erdem Varol <burakerdemvarol@gmail.com>
2020-05-17 22:20:20 +0300

    User Certificate fix. User Certificate Choice Page added.

---
+++ need-verify/copilot/src/assets/js/UserRequestCertificate.js
@@ -0,0 +1,15 @@
+module.exports = {
+  data: function () {
+
+  },
+  components: {},
+  computed: {
+  },
+  methods: {
+
+    handleRequestButtonClick () {
+      // request için gerekli fieldlardan data okunacak ve istek atılacak
+    }
+  },
+  mounted: function () {}
+}
---
+++ need-verify/copilot/src/components/UserRequestCertficate.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container style="height: 100%">
+    <el-card>
+      <el-header style="height: 5%; max-height: 65px; min-height: 40px;">
+        Certificate Request
+      </el-header>
+
+      <el-main style="height: 100%;">
+
+
+          <el-select v-model="value" placeholder="Select Institution">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+              :disabled="item.disabled">
+            </el-option>
+          </el-select>
+
+    //buradaki data creator Certificate tablosundaki institution nameler ile doldurulacak.
+
+        <el-button v-on:click="handleRequestButtonClick"  type="primary" plain>Request</el-button>
+      </el-main>
+    </el-card>
+  </el-container>
+</template>
+
+<script src="../assets/js/UserRequestCertificate.js"></script>
+
+<style scoped>
+
+</style>
